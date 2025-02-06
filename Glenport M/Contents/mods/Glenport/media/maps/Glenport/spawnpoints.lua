abbde61f83a8be14eb824a99ddc4ee7a223e8e4e
ZarkXav <ZarkXavox@gmail.com>
2025-02-06 10:03:04 -0600

    Glenport Map Files
    Main map files for Glenport.

---
+++ Glenport M/Contents/mods/Glenport/media/maps/Glenport/spawnpoints.lua
@@ -0,0 +1,39 @@
+function SpawnPoints()
+    return {
+     
+      constructionworker = { 
+      { worldX = 37, worldY = 25, posX = 150, posY = 2, posZ = 0 },
+	  { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },
+    
+      fireofficer = { 
+      { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },
+      
+      parkranger = { 
+      { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },
+    
+      policeofficer = {
+      { worldX = 37, worldY = 25, posX = 222, posY = 34, posZ = 0 },
+      { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },
+    
+      securityguard = {
+      { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },
+    
+      unemployed = { 
+      { worldX = 37, worldY = 25, posX = 274, posY = 175, posZ = 0 }, 
+	  { worldX = 37, worldY = 25, posX = 210, posY = 266, posZ = 0 }, 
+      },    	  
+       
+ 
+    }
+    
+    end
